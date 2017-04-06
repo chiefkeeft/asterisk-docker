6a6b3402d743df4c368c828eca777e7e25e9d45c
chiefkeeft <chiefkeeft@gmail.com>
2017-04-06 07:37:05 +0000

    111

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
 
 
-docker run -it --net=host -v /var/absdata/servicephone-pbx-custom-mazdasto:/etc/asterisk -v /var/absdata/servicephone-pbx-moh:/var/lib/asterisk/moh -v /var/absdata/servicephone-pbx-ivr:/var/lib/asterisk/hello servicephone-1 
+docker run -it --net=host -v /var/absdata/servicephone-pbx13:/etc/asterisk -v /var/absdata/servicephone-pbx-custom-mazdasto:/etc/asterisk -v /var/absdata/servicephone-pbx-moh:/var/lib/asterisk/moh -v /var/absdata/servicephone-pbx-ivr:/var/lib/asterisk/hello servicephone-1 
 
