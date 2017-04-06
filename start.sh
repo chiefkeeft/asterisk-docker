83edf92569400763c24c5777f794280e3c552314
chiefkeeft <chiefkeeft@gmail.com>
2017-04-06 07:06:58 +0000

    start.sh

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -x
+
+chmod 775 /etc/asterisk/
+set -e
+
+if [ -z "$ENV SIP_PORT" ]; then
+    echo "Default SIP PORT"
+else
+    echo "SIP Port changed"
+    sed -i "s/bindport=5060/bindport=$SIP_PORT/g" /etc/asterisk/sip.conf
+fi
+
+if [ -z "$ENV RTP_PORT_START" ]; then
+    echo "Default RTP Port Start"
+else
+    echo "SIP RTP Start changed"
+    sed -i "s/rtpstart=10000/rtpstart=$RTP_PORT_START/g" /etc/asterisk/rtp.conf
+fi  
+
+if [ -z "$ENV RTP_PORT_END" ]; then 
+    echo "Default RTP Port End"
+else    
+    echo "SIP RTP END changed"
+    sed -i "s/rtpend=20000/rtpend=$RTP_PORT_END/g" /etc/asterisk/rtp.conf
+fi      
+
+
+/usr/sbin/asterisk -vvvvvvv
