2048bd944324bf042f1f6bfac4a3dde6131996a1
chiefkeeft <chiefkeeft@gmail.com>
2017-04-06 07:29:44 +0000

    commit locally

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
 
 
-docker run -it --net=host -v /var/absdata/servicephone-pbx-moh:/var/lib/asterisk/moh -v /var/absdata/servicephone-pbx-ivr:/var/lib/asterisk/hello servicephone 
+docker run -it --net=host -v /var/absdata/servicephone-pbx-moh:/var/lib/asterisk/moh -v /var/absdata/servicephone-pbx-ivr:/var/lib/asterisk/hello servicephone-1 
 
