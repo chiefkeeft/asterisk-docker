b9d1f07e3f56e58d709912c6a2c2663155bad0b7
chiefkeeft <chiefkeeft@gmail.com>
2017-04-06 07:24:16 +0000

    docker mount

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
 
 
-docker run -it --net=host -v /var/absdata/setvicephone-pbx-moh:/var/lib/asterisk/moh /var/absdata/setvicephone-pbx-ivr:/var/lib/asterisk/heelo servicephone 
+docker run -it --net=host -v /var/absdata/setvicephone-pbx-moh:/var/lib/asterisk/moh -v /var/absdata/setvicephone-pbx-ivr:/var/lib/asterisk/heelo servicephone 
 
